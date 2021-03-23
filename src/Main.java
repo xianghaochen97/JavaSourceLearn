362ea355c6d8d58ed3335f8a7178de6a76561d0c
theo <xhchen971028@163.com>
2021-03-23 15:08:46 +0800

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+/**
+ * @author xhc
+ * @create 2021/3/22 15:01
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/hashmap/Test.java
@@ -0,0 +1,23 @@
+package test.hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author xhc
+ * @date 2021/3/22 15:04
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+        Map<String, Double> hashMap = new HashMap<>();
+        hashMap.put("k1", 0.1);
+        hashMap.put("k2", 0.2);
+        hashMap.put("k3", 0.3);
+        hashMap.put("k4", 0.4);
+        hashMap.put("k5", 0.5);
+        System.out.println(hashMap.toString());
+
+    }
+}
